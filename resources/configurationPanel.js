2519e8fdf4a4529a1d14c40bb20454393a82ef1d
djmapper <scottiebee@gmail.com>
2014-11-25 17:27:26 +1300

    Upload

---
+++ resources/configurationPanel.js
@@ -0,0 +1,44 @@
+{
+    "configurationSettings": [{
+        "category": "<b>Configure template</b>",
+        "fields": [{
+            "type": "webmap"
+        },{
+            "placeHolder": "Defaults to map owner",
+            "label": "Owner Text:",
+            "fieldName": "owner",
+            "type": "string",
+            "tooltip": "Defaults to map owner"
+        }, {
+            "type": "string",
+            "fieldName": "theme",
+            "tooltip": "Color theme to use",
+            "label": "Color Scheme:",
+            "options": [{
+                "label": "Chrome",
+                "value": "chrome"
+            }, {
+                "label": "Seaside",
+                "value": "seaside"
+            }, {
+                "label": "Pavement",
+                "value": "pavement"
+            }]
+        }]
+      },{
+        "category": "Tools",
+        "fields":[
+            {
+                "type": "boolean",
+                "fieldName": "search",
+                "label": "Address Finder"
+            }
+        ]
+
+    }],
+    "values": {
+        "search": true
+
+
+    }
+}
